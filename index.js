71aaf2dfc53ac24155d3ba1e341fc2dbaae06e5e
NikaMaghaldadze <113047881+NikolozMaghaldadze@users.noreply.github.com>
2024-01-30 15:03:23 +0400

    12

--- index.js
+++ index.js
@@ -68,7 +68,43 @@ yellowButtons.forEach((el) => {
       } else if (opperation === "%") {
         prevOpperand.textContent = "";
         display.textContent = modulousDivision(numberPrev, number);
+      } else if (opperation === "/") {
+        prevOpperand.textContent = " ";
+        display.textContent = division(numberPrev, number);
+      } else if (opperation === "X") {
+        prevOpperand.textContent = " ";
+        display.textContent = multiply(numberPrev, number);
+      } else if (opperation === "+") {
+        prevOpperand.textContent = " ";
+        display.textContent = add(numberPrev, number);
+      } else if (opperation === "-") {
+        prevOpperand.textContent = " ";
+        display.textContent = minus(numberPrev, number);
       }
+    } else if (el.textContent === "/") {
+      opperation = "/";
+      prevOpperand.textContent = `${number} ${opperation}`;
+      display.textContent = " ";
+      numberPrev = number;
+      number = "";
+    } else if (el.textContent === "X") {
+      opperation = "X";
+      prevOpperand.textContent = `${number} ${opperation}`;
+      display.textContent = " ";
+      numberPrev = number;
+      number = "";
+    } else if (el.textContent === "+") {
+      opperation = "+";
+      prevOpperand.textContent = `${number} ${opperation}`;
+      display.textContent = " ";
+      numberPrev = number;
+      number = "";
+    } else if (el.textContent === "-") {
+      opperation = "-";
+      prevOpperand.textContent = `${number} ${opperation}`;
+      display.textContent = " ";
+      numberPrev = number;
+      number = "";
     }
   });
 });
@@ -90,9 +126,33 @@ function stringToTypeMinus(number1, number2) {
 }
 
 function modulousDivision(number1, number2) {
-  return number1 % number2;
+  let toNumber1 = Number(number1);
+  let toNumber2 = Number(number2);
+  return toNumber1 % toNumber2;
 }
 
 function division(number1, number2) {
-  return number1 / number2;
+  let toNumber1 = Number(number1);
+  let toNumber2 = Number(number2);
+  return toNumber1 / toNumber2;
 }
+
+function multiply(number1, number2) {
+  let toNumber1 = Number(number1);
+  let toNumber2 = Number(number2);
+  return toNumber1 * toNumber2;
+}
+
+function add(number1, number2) {
+  let toNumber1 = Number(number1);
+  let toNumber2 = Number(number2);
+  return toNumber1 + toNumber2;
+}
+
+function minus(number1, number2) {
+  let toNumber1 = Number(number1);
+  let toNumber2 = Number(number2);
+  return toNumber1 - toNumber2;
+}
+
+console.log(2.5 + 2.5);
